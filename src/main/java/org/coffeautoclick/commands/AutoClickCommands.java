92c5d49866548414f037beda8f46a185e03e3b69
BearDeveloper <dr.polar099@gmail.com>
2023-04-25 12:37:29 -0300

    EU SOU FODA TIO, CHEGA QUE VAI TOMAR UM EMOJI NA CARA

--- src/main/java/org/coffeautoclick/commands/AutoClickCommands.java
+++ src/main/java/org/coffeautoclick/commands/AutoClickCommands.java
@@ -6,12 +6,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ComplexLivingEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.coffeautoclick.Coffe_Autoclick;
 
 public class AutoClickCommands implements CommandExecutor {
@@ -23,17 +25,26 @@ public class AutoClickCommands implements CommandExecutor {
             if (cmd.getName().equalsIgnoreCase("autoclick")) {
                 if (p.hasPermission("autoclick.use")) {
                     if (args.length > 0 && args[0].equalsIgnoreCase("use")) {
-                    double d = config.getDouble("autoclick.blocks-of-range");
-                    double dmg = config.getDouble("autoclick.damage");
-                    p.sendMessage(ChatColor.GREEN + "Hitando...");
-                    for (Entity target : p.getNearbyEntities(d, d, d)) {
-                        if (target instanceof LivingEntity) {
-                            ((LivingEntity) target).damage(dmg);
-                        } else {
-                            p.sendMessage(ChatColor.RED + "Poxa garotinho, Não pode hitar o amiguinho não...");
-                            return true;
-                        }
-                    }
+                        double d = config.getDouble("autoclick.blocks-of-range");
+                        double dmg = config.getDouble("autoclick.damage");
+                        p.sendMessage(ChatColor.GREEN + "Hitando...");
+                        int time = config.getInt("autoclick.time-of-click");
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                for (Entity target : p.getNearbyEntities(d, d, d)) {
+                                    if (target instanceof LivingEntity) {
+                                        ((LivingEntity) target).damage(dmg);
+                                    } else if (target instanceof Player){
+                                        p.sendMessage(ChatColor.RED + "Poxa garotinho, Não pode hitar o amiguinho não...");
+                                        break;
+                                    }
+                                    else if(target instanceof ComplexLivingEntity) {
+                                        ((ComplexLivingEntity) target).damage(dmg);
+                                    }
+                                }
+                            }
+                        }.runTaskLater(Coffe_Autoclick.getInstance(), 20L * time);
                 }
                 } else {
                     p.sendMessage(ChatColor.RED + "Você não tem permissão!");
