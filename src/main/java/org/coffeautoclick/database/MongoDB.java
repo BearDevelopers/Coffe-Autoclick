130df902b6630b8b76ce5d1e181df04d1fe9b5dc
BearDeveloper <dr.polar099@gmail.com>
2023-04-25 19:05:11 -0300

    Foda

--- src/main/java/org/coffeautoclick/database/MongoDB.java
+++ src/main/java/org/coffeautoclick/database/MongoDB.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class MongoDB {
     private static MongoClient mongoClient;
@@ -13,12 +15,17 @@ public class MongoDB {
     private static MongoCollection<Document> collection;
 
     public static void connection() {
-        String user = "localhost";
-        int port = 27111;
-        String url = "mongodb://localhost:27017/";
-        mongoClient = MongoClients.create(url);
-        database = mongoClient.getDatabase("Coffe-Autoclick");
-        collection = database.getCollection("souls");
+        try {
+            String user = "localhost";
+            int port = 27111;
+            String url = "mongodb://localhost:27017/";
+            mongoClient = MongoClients.create(url);
+            database = mongoClient.getDatabase("Coffe-Autoclick");
+            collection = database.getCollection("souls");
+        }
+        catch (Exception e) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Erro conectando database!");
+        }
     }
 
     public static MongoCollection<Document> getDocument() {
